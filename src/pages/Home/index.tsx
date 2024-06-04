ee49271f1ff53f8b25b0e7d9e45cf08d11bf39c4
Maju Facirolli <majufacirolli2004@hotmail.com>
2024-06-04 12:35:13 -0300

    feat: create Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,58 @@
+import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react"
+import { Heading, Hero, HeroContent, Info } from "./styles"
+import { useTheme } from "styled-components"
+
 export function Home() {
-    return <h1>Home</h1>
+    const theme = useTheme()
+    return (
+        <>
+            <Hero>
+                <HeroContent>
+                    <Heading>
+                        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+                        <p>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</p>
+                    </Heading>
+                    <Info>
+                        <div>
+                            <ShoppingCart 
+                                size={32} 
+                                weight="fill" 
+                                color={theme.background} 
+                                style={{background: theme["yellow-dark"]}}
+                            />
+                            <span>Compra simples e segura</span>
+                        </div>
+                        <div>
+                            <Package 
+                                size={32} 
+                                weight="fill"
+                                color={theme.background} 
+                                style={{background: theme["base-text"]}}
+                            />
+                            <span>Embalagem mantém o café intacto</span>
+                        </div>
+                        <div>
+                            <Timer 
+                                size={32} 
+                                weight="fill"
+                                color={theme.background} 
+                                style={{background: theme.yellow}}
+                            />
+                            <span>Entrega rápida e rastreada</span>
+                        </div>
+                        <div>
+                            <Coffee 
+                                size={32} 
+                                weight="fill" 
+                                color={theme.background} 
+                                style={{background: theme.purple}}
+                            />
+                            <span>O café chega fresquinho até você</span>
+                        </div>
+                    </Info>
+                </HeroContent>
+                <img src="/images/hero.svg" alt="" />
+            </Hero>
+        </>
+    )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import { mixins } from "../../styles/mixins";
+
+export const Hero = styled.section`
+    min-height: calc(100vh - 6.8rem);
+    background-image: url(/images/hero-bg.svg);
+    background-repeat: no-repeat;
+    background-size: cover;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-evenly;
+`
+
+export const HeroContent = styled.div`
+    width: 36.5rem;
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+`
+
+export const Heading = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    h1 {
+        ${mixins.fonts.titleXL}
+        color: ${props => props.theme["base-title"]};
+    }
+
+    p {
+        ${mixins.fonts.textL}
+    }
+`
+
+export const Info = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-row-gap: 1.25rem;
+
+    div {
+        display: flex;
+        gap: 0.5rem;
+        align-items: center;
+    }
+
+    svg {
+        border-radius: 50%;
+        padding: 0.5rem;
+    }
+`
